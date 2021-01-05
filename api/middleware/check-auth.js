f95305972f9cbda96af2a8b65bf7b03d05bc1e67
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2021-01-05 12:40:06 +0530

    Commit 4
    Completed Login/SignUp API

---
+++ api/middleware/check-auth.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+module.exports =  (req,res,next) => {
+  //verify jwt(token,key): it throws error if not  verified thus the try-catch block
+  try{
+    const token = req.headers.authorization.split(" ")[1]; //headers come as BEARER {TOKEN}
+    const decoded = jwt.verify(token,process.env.JWT_KEY);
+    req.userData = decoded;
+    next();
+  }
+  catch(error)
+  {
+      return res.status(401).json({
+        message:"Auth Failed"
+      });
+  }
+};
